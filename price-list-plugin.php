5d57b17a41bb8660847c9dedd9f31bbbe59f89fb
raucher <myplace4spam@gmail.com>
2013-09-14 12:31:28 +0300

    Initial commit

---
+++ price-list-plugin.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * Plugin Name: Price List Plugin
+ * Plugin URI:
+ * Description: Alows you to create price-lists and embed them with a shortcode
+ * Version: 1.0
+ * Author: raucher
+ * Author URI: https://github.com/raucher
+ * License: GPLv2
+ */
+
+/********************************************************************************
+    Copyright YEAR PLUGIN_AUTHOR_NAME (email : PLUGIN AUTHOR EMAIL)
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+********************************************************************************/
+
+defined('PLP_URL') or define('PLP_URL', plugin_dir_url(__FILE__));
+defined('PLP_PATH') or define('PLP_PATH', plugin_dir_path(__FILE__));
+
+require_once 'includes/PriceListPlugin.php';
+
+add_action('init', array(new PriceListPlugin, 'init'));
+
---
+++ uninstall.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Makc
+ * Date: 9/12/13
+ * Time: 2:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
