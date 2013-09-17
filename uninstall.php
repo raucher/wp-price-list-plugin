787000b428b6fac11b8e26d98a3886412786fe0f
raucher <myplace4spam@gmail.com>
2013-09-17 12:31:10 +0300

    uninstall.php changed

--- uninstall.php
+++ uninstall.php
@@ -1,12 +1,10 @@
 <?php
-if(!get_option('plp_is_installed'))
-    return;
-
-$samplePost = get_posts(array(
-    'name' => 'plp-sample-price-list',
+$priceLists = get_posts(array(
     'post_type' => 'price_list',
-    'posts_per_page' => 1,
 ));
-wp_delete_post($samplePost[0]->ID, true);
+
+foreach ($priceLists as $priceList) {
+    wp_delete_post($priceList->ID, true);
+}
 
 delete_option('plp_is_installed');
